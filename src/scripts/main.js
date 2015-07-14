97e71646f284e567c37e0611b1a1e7c7841193ac
Ali Hammoud <ali.hammoud.lb@gmail.com>
2015-07-14 23:25:06 +0300

    Image scales properly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
-/*jslint eqeq: true, nomen: true, plusplus: true, unparam: false, sloppy: true, indent: 2, maxlen: 80*/
+/*jslint devel: true, bitwise: true, continue: true, plusplus: true,
+unparam: false, sloppy: true,  vars: true, white: false, indent: 2,
+maxlen: 80*/
 
 var img;
 
@@ -34,74 +36,145 @@ $(document).ready(function () {
 
   /* drop-related */
 
-  function fitWidth(selector, aspect, winWidth) {
+  function fitOrigSize(selector, imageWidth, imageHeight) {
     
-    $(selector).attr('width', winWidth);
-    $(selector).attr('height', $(selector).attr('width') / aspect);
+    $(selector).attr('width', imageWidth);
+    $(selector).attr('height', imageHeight);
     
   }
   
+  function fitWidth(selector, aspect, winWidth) {
+
+    $(selector).attr('width', winWidth);
+    $(selector).attr('height', $(selector).attr('width') / aspect);
+
+  }
+
   function fitHeight(selector, aspect, winHeight) {
-    
+
     $(selector).attr('height', winHeight);
     $(selector).attr('width', $(selector).attr('height') * aspect);
     
   }
+
+  function checkTopBar(selector) {
+    
+    if ($(selector).attr('height') > (window.innerHeight - 80)) {
+      
+      var
+        diff = $(selector).attr('height') - (window.innerHeight - 80),
+        val = 0.5 + (1 - (diff / 80));
+      
+      $('#topBar').css({opacity: val});
+      
+    } else {
+      
+      $('#topBar').css({opacity: 1});
+      
+    }
+    
+  }
   
   function fitLargestDim(selector, aspect) {
-    
+
     var
       winWidth = window.innerWidth,
-      winHeight = window.innerHeight;
-    
-    //check if element height larger than win height
-    if ($(selector).attr('height') > winHeight &&
-        img.height > winHeight
-        ) {
-      
-      fitHeight(selector, aspect, winHeight);
+      winHeight = window.innerHeight,
+      width = $(selector).attr('width'),
+      height = $(selector).attr('height');
 
-    } else {
+    if (img.width > winWidth && img.height < winHeight) {
       
-      fitHeight(selector, aspect, winHeight);
-      
-    }
-
-    //check if element width larger than win width
-    if ($(selector).attr('width') > winWidth &&
-        img.width > winWidth
-        ) {
-
       fitWidth(selector, aspect, winWidth);
-
-    } else {
+      
+    }
+    
+    if (img.width > winWidth && img.height > winHeight) {
+      
+      var screenAspect = winWidth / winHeight;
+      
+      if (screenAspect > aspect) {
+        
+        fitHeight(selector, aspect, winHeight);
+        
+      }
+      
+      if (screenAspect < aspect) {
+        
+        fitWidth(selector, aspect, winWidth);
+        
+      }
+      
+    }
+    
+    if (img.width > winWidth && img.height === winHeight) {
+      
+      fitWidth(selector, aspect, winWidth);
+      
+    }
+    
+    if (img.width < winWidth && img.height < winHeight) {
+      
+      fitOrigSize(selector, img.width, img.height);
+      
+    }
+    
+    if (img.width < winWidth && img.height > winHeight) {
       
       fitHeight(selector, aspect, winHeight);
       
     }
     
+    if (img.width < winWidth && img.height === winHeight) {
+      
+      fitOrigSize(selector, img.width, img.height);
+      
+    }
+    
+    if (img.width === winWidth && img.height > winHeight) {
+      
+      console.log("same width, scale height");
+      fitHeight(selector, aspect, winHeight);
+      
+    }
+    
+    if (img.width === winWidth && img.height < winHeight) {
+      
+      fitOrigSize(selector, img.width, img.height);
+      
+    }
+    
+    checkTopBar(selector);
+
   }
 
-  function checkSmallestDim(selector) {
+  function fitElement(selector) {
 
-    var
-      aspect;
-
-    if (img != null) {
-
+    try {
+      
+      var aspect;
+      
       aspect = img.width / img.height;
+      fitLargestDim(selector, aspect);
 
+    } catch (e) {
+      
+      //console.warn("img not set yet");
+      console.warn(e);
+      
     }
 
-    fitLargestDim(selector, aspect);
-
   }
-
+  
   function readImageFile(file) {
 
     var
       reader = new FileReader(),
+      fileSizeRaw = file.size,
+      fileSizeMB,
       imageType = /image/;
+
+    fileSizeMB = fileSizeRaw / (1024 * 1024);
 
     if (file.type.match(imageType)) {
 
@@ -116,8 +189,20 @@ $(document).ready(function () {
           .attr('width', img.width)
           .attr('height', img.height)
           .attr('src', img.src);
+        
+        try {
+          
+          sessionStorage.setItem('editImg', img.src);
+          console.log("Image uploaded. Size: " +
+                      fileSizeMB.toPrecision(2) + " MB");
+          
+        } catch (exception) {
 
-        checkSmallestDim('#dispImg');
+          console.warn("Quota exceeded");
+
+        }
+
+        fitElement('#dispImg');
 
       };
 
@@ -127,7 +212,7 @@ $(document).ready(function () {
 
     } else {
       // Handle error: file format specified by user is not supported
-      alert("Invalid format");
+      alert("Invalid file format");
 
     }
 
@@ -159,8 +244,30 @@ $(document).ready(function () {
     }
   );
 
+  $('#dragDrop').click(function () {
+
+    $('#imgInput').trigger('click');
+
+  });
+
+  $('#imgInput').change(function () {
+
+    var
+      input = document.getElementById('imgInput'),
+      inputFileList = input.files;
+
+    upload(inputFileList);
+
+  });
+
   window.onresize = function () {
-    checkSmallestDim('#dispImg');
+    fitElement('#dispImg');
+  };
+
+  window.onunload = function () {
+    
+    sessionStorage.clear();
+    
   };
 
 });
